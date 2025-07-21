3dc72ac093a1198faebeb35c82e7e1b8c84b2518
Philipp Lackner <mail@pl-coding.com>
2025-07-21 12:32:29 +0200

    Implement JWT service

--- user/src/main/kotlin/com/plcoding/chirp/api/exception_handling/AuthExceptionHandler.kt
+++ user/src/main/kotlin/com/plcoding/chirp/api/exception_handling/AuthExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.plcoding.chirp.api.exception_handling
 
+import com.plcoding.chirp.domain.exception.InvalidTokenException
 import com.plcoding.chirp.domain.exception.UserAlreadyExistsException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -17,6 +18,15 @@ class AuthExceptionHandler {
         e: UserAlreadyExistsException
     ) = mapOf(
         "code" to "USER_EXISTS",
+        "message" to e.message
+    )
+
+    @ExceptionHandler(InvalidTokenException::class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    fun onInvalidToken(
+        e: InvalidTokenException
+    ) = mapOf(
+        "code" to "INVALID_TOKEN",
         "message" to e.message
     )
 
